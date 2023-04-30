7778d2bb163c7ef373ae6f1766a3def79516dd6b
chandrajob365 <chandrajob365@gmail.com>
2023-04-30 14:19:43 +0530

    Initial commit

---
+++ packages/resturant-dashboard/__tests__/resturant-dashboard.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const resturantDashboard = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(resturantDashboard(), 'Hello from resturantDashboard');
+console.info('resturantDashboard tests passed');
---
+++ packages/resturant-dashboard/lib/resturant-dashboard.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = resturantDashboard;
+
+function resturantDashboard() {
+  return 'Hello from resturantDashboard';
+}
