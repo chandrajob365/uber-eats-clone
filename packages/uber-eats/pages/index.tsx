1ac0a196a26a4a7d0244890462d03cc2f461f81c
chandrajob365 <chandrajob365@gmail.com>
2023-04-30 15:37:04 +0530

    Basic setup of uber eats next app
    Basic setup of uber eats next app
    Remove unwanted files and folder created out of default next template

---
+++ packages/uber-eats/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import React from 'react'
+
+export default function Home({ allPostsData }) {
+  return (
+    <div >
+      <Head>
+        <title>Hello World</title>
+      </Head>
+    </div>
+  )
+}
